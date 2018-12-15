72525ea082decb43431e3766635a53ff22aab677
sapelkinAV <al.vi.sapelkin@gmail.com>
2018-12-15 19:23:58 +0300

    fix relative path variables

--- link.windows.ps1
+++ link.windows.ps1
@@ -4,15 +4,15 @@ param (
 )
 
 
-
+$java_path = "$td/example/java"
+$java_compile_destination = "$java_path/bin"
 Push-Location $td/example/java/build
 try {
-    dir
 
     $cmakeArguments = @(
     '-DCMAKE_BUILD_TYPE=Release'
-    '-DTd_DIR=././td/lib/cmake/Td'
-    '-DCMAKE_INSTALL_PREFIX:PATH=..'
+    "-DTd_DIR=$java_path/td/lib/cmake/Td"
+    "-DCMAKE_INSTALL_PREFIX:PATH=$java_compile_destination"
     '..'
     )
     $cmakeBuildArguments = @(
