ae2ba80fb6959d911345d7a2ad286fe6158fbcfc
Alexander Sapelkinn <al.vi.sapelkin@gmail.com>
2018-12-31 13:47:04 +0300

    revert to pwsh

--- build.osx.ps1
+++ build.osx.ps1
@@ -2,16 +2,52 @@ param (
     $td = "$PSScriptRoot/td"
 )
 
+$build_path = "$td/build"
+$java_path = "$td/example/java"
+$java_compile_destination = "$java_path/bin"
 
 New-Item -Type Directory $td/build
 New-Item -Type Directory $td/example/java/build
 
 
-cd ./build
-cmake -DCMAKE_BUILD_TYPE=Release -DTD_ENABLE_JNI=ON -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl/ -DCMAKE_INSTALL_PREFIX:PATH=../example/java/td ..
-cmake --build . --target install
+$ErrorActionPreference = 'Stop'
+
+if (-not (Test-Path $td/build)) {
+    New-Item -Type Directory $td/build
+}
+if (-not (Test-Path $td/example/java/build)) {
+    New-Item -Type Directory $td/example/java/build
+}
 
 
-cd ../example/java/build
-cmake -DCMAKE_BUILD_TYPE=Release -DTd_DIR=././td/lib/cmake/Td/ -DCMAKE_INSTALL_PREFIX:PATH=.. ..
-cmake --build . --target install
+Push-Location $build_path
+try {
+
+    $cmakeArguments = @(
+        '-DCMAKE_BUILD_TYPE=Release'
+        '-DOPENSSL_ROOT_DIR=/usr/local/opt/openssl/'
+        "-DCMAKE_INSTALL_PREFIX:PATH=../example/java/td"
+        '-DTD_ENABLE_JNI=ON'
+        '..'
+    )
+    $cmakeBuildArguments = @(
+        '--build'
+        '.'
+    )
+
+    cmake $cmakeArguments
+    if (!$?) {
+        throw 'Cannot execute cmake'
+    }
+
+    cmake $cmakeBuildArguments
+    if (!$?) {
+        throw 'Cannot execute cmake --build'
+    }
+} finally {
+    Pop-Location
+}
+
+
+
+
