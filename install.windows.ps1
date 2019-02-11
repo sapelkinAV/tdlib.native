0c9d24a394f43c169204216b8554d798a0246eba
Alexander Sapelkinn <al.vi.sapelkin@gmail.com>
2019-02-11 17:05:23 +0300

    yet another try

--- install.windows.ps1
+++ install.windows.ps1
@@ -3,4 +3,10 @@ $ErrorActionPreference = 'Stop'
 choco install gperf
 if (!$?) { throw 'Cannot install dependencies from choco' }
 
+choco install vcpkg
+if (!$?) { throw 'Cannot install vcpkg from choco' }
+
+vcpkg install openssl:x64-windows openssl:x86-windows zlib:x64-windows zlib:x86-windows
+if (!$?) { throw 'Cannot install vcpkg dependencies' }
+
 git submodule update --init --recursive
